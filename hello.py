325e38b7c0a2546735a14f884c44ac7b9832db6b
brian <x169@qq.com>
2016-01-20 09:26:53 +0800

    helloworld

---
+++ hello.py
@@ -0,0 +1,23 @@
+#print("hello world!")
+#a = 1
+#b = 2
+#c = a+b
+#print(c)
+c1="jikexueyuan"
+c2=c1[0]
+c3=c1[7]
+c4=c1[:2]
+c5=c1[2:]
+c6=c1[4:7]
+c=[2,3,4,5,6]
+for j in enumerate(c):
+	print(j)
+	
+	l = [1,2,3]
+for index,val in enumerate(l):
+    print('index is %d val is %d',(index,val))
+    
+    a=set("afafafaf")
+print(a)
+#n=set(a)
+#print(n)
