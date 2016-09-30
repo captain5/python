ab95994355ca59ad011cd457b15ceeb0a422c503
captain5 <captain5@users.noreply.github.com>
2016-09-30 17:00:09 +0800

    Create string_ex.py

---
+++ module/string_ex.py
@@ -0,0 +1,14 @@
+>>> m = "Hello world!"
+>>> m.find(H)
+>>> m.find('H')
+0
+>>> m.find('d')
+10
+
+str = ’0123456789′
+print str[0:3] #截取第一位到第三位的字符
+
+>>> abc = 'Hello world! hi'
+>>> abc.split()
+['Hello', 'world!', 'hi']
+
