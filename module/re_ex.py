115c321007a719c232be60bd5202f97d5015c0a9
captain5 <captain5@users.noreply.github.com>
2016-09-30 17:02:23 +0800

    Update re_ex.py

--- module/re_ex.py
+++ module/re_ex.py
@@ -5,6 +5,15 @@
 >>> print(m.group(0))
 output_1986
 
+# res is a string
+>>> res
+'{"url":"/cgi-bin/luci/;stok=c9ec12465cd921116fa7e7dc01d45832/web/home","token":"c9ec12465cd921116fa7e7dc01d45832","code":0}'
+>>> re.search('url":"(.*)","token',res).group(1)
+'/cgi-bin/luci/;stok=c9ec12465cd921116fa7e7dc01d45832/web/home'
+>>> res[0]
+'{'
+>>> res[65:69]
+'home'
 
 >>> import re
 >>> m = re.search('(?<=abc)def', 'abcdef')
