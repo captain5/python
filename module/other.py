e14adbfed217400913911eaa114a65e8b6d09f32
captain5 <captain5@users.noreply.github.com>
2016-09-30 10:02:34 +0800

    Update other.py

--- module/other.py
+++ module/other.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+__author__ = "captain5"
+
+
+data = "abc";
+import hashlib;
+hashlib.md5(data).hexdigest()
+hashlib.sha1(data).hexdigest()
+
 >>> import random
 >>> random.random()
 0.42428021348064426
+
+
