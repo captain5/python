8c18ab6bd4f52093c281591c385812c27646c00a
Captain5 <x169@qq.com>
2016-10-09 11:06:50 +0800

    add vamei socket

---
+++ vamei/socket_client.py
@@ -0,0 +1,20 @@
+# Written by Vamei
+# Client side
+import socket
+
+# Address
+HOST = '10.237.100.153'
+PORT = 8000
+
+request = 'can you hear me?'
+# configure socket
+s       = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+# send message
+s.sendall(request.encode())
+# receive message
+reply   = s.recv(1024)
+print('reply is: ',reply.decode())
+# close connection
+s.close()
