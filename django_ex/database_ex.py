d9c43bac0522a3fe667cf429e4eb077db7760c88
captain5 <captain5@users.noreply.github.com>
2017-07-28 16:55:18 +0800

    Update database_ex.py

--- django_ex/database_ex.py
+++ django_ex/database_ex.py
@@ -1,8 +1,10 @@
-数据库测试能连接也能查到数据
->>> from django.db import connection
->>> cursor = connection.cursor()
->>> cursor.execute("select * from west_character");
+#数据库测试，能连接，也能查到数据
+from django.db import connection
+
+#片断不能执行：
+cursor = connection.cursor()
+cursor.execute("select * from west_character");
 3L
->>> wc=cursor.fetchall();
->>> print wc
+wc=cursor.fetchall();
+print wc
 ((1L, u'Vamei'), (2L, u'Django'), (3L, u'John'))
